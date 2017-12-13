e41f5782f41b40cba6f4b25c69c5ce0b76ff490e
Scoudem <tvvaalen@gmail.com>
2017-12-13 16:07:16 +0100

    Single file reading

--- visualisation/render.py
+++ visualisation/render.py
@@ -40,15 +40,20 @@ class CloneClass:
         return CloneClass(data['class'], [FileEntry.from_json(j) for j in data['files']])
 
 
-def parse_clone_file(clone_file: str) -> CloneClass:
+def parse_clone_file(clone_file: str) -> List[CloneClass]:
     with open(clone_file, 'r') as file:
         data = json.load(file)
-        return CloneClass.from_json(data)
+        return [CloneClass.from_json(entry) for entry in data]
 
 
 def read_all_clones() -> List[CloneClass]:
     clone_files = glob.glob(os.path.expanduser('~') + '/clones/*.txt')
-    return [parse_clone_file(f) for f in clone_files]
+
+    matches = []
+    for f in clone_files:
+        matches += parse_clone_file(f)
+
+    return matches
 
 
 def extract_unique_names() -> List[str]:
